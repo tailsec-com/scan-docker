eee063d3299d6df98146f1b112d96d3498cf7713
Mark Topper <mark@ulties.com>
2026-05-12 20:58:33 +0200

    Add jest.config.js with ESM support

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: { '^(\\.{1,2}/.*)\\.js$': '$1' },
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true }],
+  },
+};
